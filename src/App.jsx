a24c8f2aa57c7fd2db0740d4834920ad321c837c
Nikilesh Raj <nikileshnatraj@gmail.com>
2024-01-04 19:53:56 +0530

    Modified to switch between screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined, // SelectedProject will be used to store the id
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((previousState) => {
+      return { ...previousState, selectedProjectId: null };
+    });
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  } else {
+    // Content will hold the selected project.
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
